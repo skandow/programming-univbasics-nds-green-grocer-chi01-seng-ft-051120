534ca720e4d49c011c1c8bc6f9511a9eec8b2e90
Steven Kandow <kando1sr@gmail.com>
2020-03-14 20:50:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,35 @@
 def find_item_by_name_in_collection(name, collection)
+  collection_index = 0 
+  while collection_index < collection.length do 
+    if collection[collection_index][:item] == name
+      sought_item_info = collection[collection_index]
+    end 
+    collection_index += 1 
+  end
+  sought_item_info
   # Implement me first!
   #
   # Consult README for inputs and outputs
 end
 
 def consolidate_cart(cart)
+  cleaned_cart = []
+  cart_index = 0
+  while cart_index < cart.length do 
+    compare_index = 0 
+    while compare_index < cart.length do 
+      item_name = cart[compare_index][:name]
+      if !cleaned_cart[item_name]
+        cleaned_cart << cart[cart_index]
+        cleaned_cart[cart_index][:count] = 1
+      else 
+        cleaned_cart[]
+      end
+        compare_index += 1
+    end
+    cart_index += 1
+  end
+  cleaned_cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
@@ -12,18 +37,62 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  coupon_check = 0
+  while coupon_check < cart.length do 
+    coupon_compare = 0
+    while coupon_compare < coupons.length do
+    if cart[coupon_check][:item] == coupons[coupon_compare][:item] && cart[coupon_check][:count] >= coupons[coupon_compare][:num]
+      new_cost = coupons[coupon_compare][:cost] / coupons[coupon_compare][:num]
+      non_discounted_amount = cart[coupon_check][:count] % coupons[coupon_compare][:num]
+      discounted_amount = cart[coupon_check][:count] - non_discounted_amount
+      couponed_item = cart[coupon_check]
+      couponed_item[:item] = couponed_item[:item] + " " + "W/COUPON" 
+      couponed_item[:price] = new_cost
+      couponed_item[:count] = discounted_amount
+      cart[coupon_check][:count] = non_discounted_amount
+      cart << couponed_item
+    end 
+    coupon_compare += 1
+  end 
+  coupon_check += 1
+  end 
+  cart 
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
+  clean_cart = consolidate_cart(cart)
+  cart_counter = 0 
+  while cart_counter < clean_cart.length do 
+    if clean_cart[cart_counter][:clearance] == true
+      clean_cart[cart_counter][:price] = (clean_cart[cart_counter][:price] * 0.80).round(2)
+    end
+    cart_counter += 1
+  end
+  clean_cart 
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
+  cleanest_cart = consolidate_cart(cart)
+  cleanest_cart_with_coupons = apply_coupons(cleanest_cart, coupons)
+  cleanest_cart_all_discounts = apply_clearance(cleanest_cart_with_coupons)
+  cart_counting = 0 
+  final_amount = 0 
+  while cart_counting < cleanest_cart_all_discounts.length do
+    individual_item_cost = cleanest_cart_all_discounts[cart_counting][:count] * cleanest_cart_all_discounts[cart_counting][:price]
+    final_amount += individual_item_cost
+    cart_counting += 1
+  end
+  if final_amount > 100
+    final_amount = final_amount * 0.90
+  end 
+  final_amount 
   # Consult README for inputs and outputs
   #
   # This method should call
